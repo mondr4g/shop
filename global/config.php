5ba0ddcaa68599cd77b1423e42c033292c1900cf
amaury <amauryromohdz9@gmail.com>
2020-12-09 15:24:37 -0600

    aaaa

---
+++ global/config.php
@@ -0,0 +1,8 @@
+<?php
+    define("KEY","prueba_carro");
+    define("COD","AES-128-ECB");
+    define("SERVER","localhost");
+    define("USUARIO","amaury");
+    define("PASSWORD","ISCisthebest9");
+    define("BD","_store_");
+?>
